b72444c50b0580b86a9a024fb9675a495ef660a1
Jaime Pineda <jpin730@pm.me>
2026-03-19 00:19:27 -0600

    feat: :sparkles: add demonstration of primitive and wrapper arrays in Main class

--- src/dataStructures/Main.java
+++ src/dataStructures/Main.java
@@ -14,5 +14,28 @@ public class Main {
         int[] array2 = new int[100];
 
         System.out.println("Length of array2: " + array2.length);
+
+        // Primitive Arrays vs Wrapper Arrays
+        int[] primitiveArray = new int[5];
+
+        primitiveArray[0] = 10;
+        primitiveArray[1] = 20;
+
+        System.out.print("Elements of primitiveArray: ");
+        for (int number : primitiveArray) {
+            System.out.print(number + " ");
+        }
+        System.out.println();
+
+        Integer[] wrapperArray = new Integer[5];
+
+        wrapperArray[0] = 30;
+        wrapperArray[1] = 40;
+
+        System.out.print("Elements of wrapperArray: ");
+        for (Integer number : wrapperArray) {
+            System.out.print(number + " ");
+        }
+        System.out.println();
     }
 }
